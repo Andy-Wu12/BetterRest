7b7153a3946ae5c9252f367b5c81dd6987486a1b
Andy Wu <awpats12@gmail.com>
2022-12-08 17:35:21 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andy Wu on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# BetterRest - 100 Days of Swift
+
+## Fourth project in iOS / Mac OS X development journey
+
+## See the [blog](https://www.hackingwithswift.com/books/ios-swiftui/betterrest-introduction) that I am following
