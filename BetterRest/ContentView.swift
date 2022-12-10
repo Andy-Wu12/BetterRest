ec2c97b7b64782898881c79650749fb81e6fc481
Andy Wu <awpats12@gmail.com>
2022-12-09 20:17:35 -0500

    Completed forms for hours of sleep / wakeup time, set up ML model with example data

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -7,13 +7,23 @@
 
 import SwiftUI
 
+// DateComponents let us read of write specific parts of a date rather than the whole thing
+// If we wanted a date that represented 8am today,
+
+//var components = DateComponents()
+//components.hour = 8
+//components.minute = 0
+//let date = Calendar.current.date(from: components)
+
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+                .labelsHidden()
         }
         .padding()
     }
