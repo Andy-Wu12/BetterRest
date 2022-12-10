20ecdf621c82cbfbadd01d2a42c5733e8ba91489
Andy Wu <awpats12@gmail.com>
2022-12-09 21:44:35 -0500

    Connected SwiftUI to Core ML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreML
 
 // DateComponents let us read of write specific parts of a date rather than the whole thing
 // If we wanted a date that represented 8am today,
@@ -18,14 +19,64 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-                .labelsHidden()
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
-        .padding()
+    }
+    
+    func calculateBedtime() {
+        // Core ML can throw errors in loading model and/or when we ask for predictions
+        do {
+            // Set up ML model
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            // Convert wakeTime to double for use in model prediction request
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            // Request prediction
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            // Convert predicted sleep time back to Date
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
