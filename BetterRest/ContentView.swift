ab9f85f1ad4e24e798cdaaa32c5610aee6a9f14d
Andy Wu <awpats12@gmail.com>
2022-12-10 19:16:43 -0500

    Challenges 1 and 2 completed

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -19,7 +19,7 @@ import CoreML
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = defaultWakeTime
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 0
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -35,23 +35,22 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section("When do you want to wake up?") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                        
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+                Section("Desired amount of sleep") {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                Section("Daily coffee intake") {
+                    Picker("Cups of coffee", selection: $coffeeAmount) {
+                        Text("1 cup")
+                        ForEach(2 ..< 20) {
+                            Text("\($0) cups")
+                        }
+                    }
                 }
                 
             }
